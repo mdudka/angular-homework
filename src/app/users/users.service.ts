1c19d2584a0ca62c294524871546f8b37c5df83b
Mikhail Dudka <deanwenchester13@gmail.com>
2023-04-29 00:31:39 +0300

    part 2: minor fixes (changed amount of users to display with pipe and changed style cards gallery a little bit

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
-import {IUser} from "./IUser";
-import {Observable} from "rxjs";
+import { IUser } from './IUser';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
   usersUrl = 'https://jsonplaceholder.typicode.com/users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUsers(): Observable<IUser[]> {
-    return this.http.get<IUser[]>(this.usersUrl);
+    return this.http
+      .get<IUser[]>(this.usersUrl)
+      .pipe(map((users: IUser[]) => users.slice(0, 4)));
   }
 
   addUser(user: IUser): Observable<IUser> {
-    return this.http.post<IUser>(this.usersUrl, user)
+    return this.http.post<IUser>(this.usersUrl, user);
   }
 
   deleteUser(id: number): Observable<unknown> {
